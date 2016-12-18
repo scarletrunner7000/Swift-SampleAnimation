68710d271c238a4344bf251d58a226a2fb91154c
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-12-19 05:14:28 +0900

    study

--- SampleAnimation/ViewController.swift
+++ SampleAnimation/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapRoundButton(sender: AnyObject) {
+        let toValue = 20
+        // アニメーションの種類を指定
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        // タイミング
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionDefault)
+        // 開始値、目標値、時間を指定
+        animation.fromValue = 0
+        animation.toValue = toValue
+        animation.duration = 1
+        // アニメーションをレイヤーに追加
+        targetView.layer.addAnimation(animation, forKey: "cornerRadius")
+        // 変更後も角丸を維持
+        targetView.layer.cornerRadius = 20
+
+    }
+
+    @IBAction func tapEraseButton(sender: AnyObject) {
+        targetView.alpha = 1
+        UIView.animateWithDuration(1.0) { () -> Void in
+            self.targetView.alpha = 0
+        }
+    }
 
 }
 
